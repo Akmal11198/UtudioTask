dd60a4572d585caf8f5cb5ed634975f981ead1e9
akmalsalem <akmalsalem@gmail.com>
2021-05-27 11:28:31 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button,Container} from "react-bootstrap";
 
 function App() {
+  const [num,setNum]=useState(0);
+  const click=()=>{
+    if (num<5)
+      setNum(num+1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{padding:10,marginTop:10,textAlign:"center",display:"flex",flexDirection:"column"}}>
+      <Button variant="outline-success" size="lg" style={{marginTop:40}} onClick={click} disabled={num===5} >{num}</Button>
+      {(num>0)&&(<Button variant="link" size="lg" style={{marginTop:40}} onClick={()=>{setNum(0)}}>Reset</Button>)}
+    </Container>
   );
 }
 
